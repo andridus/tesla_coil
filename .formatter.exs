e52411cb8e52b3614d202955809382020573075a
andremaker <andr.crono@hotmail.com>
2021-12-03 04:59:42 -0400

    feat: add route macros to locals without parens

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,16 @@
-# Used by "mix format"
+route_macros =
+  [:get, :post, :put, :patch, :delete, :options, :connect, :trace, :head]
+  |> Enum.map(&{&1, 3})
+
+# using a second variable even if the value is the same for now, for the sake of maintainability
+# its a little better to understand when reading and add new values to the list in future
+
+locals_without_parens = route_macros
+
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  export: [
+    locals_without_parens: locals_without_parens
+  ],
+  locals_without_parens: locals_without_parens
 ]
