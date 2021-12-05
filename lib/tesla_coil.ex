cfe7e75e7b06ec8ddb6813d5dffaefc751aff589
andremaker <andr.crono@hotmail.com>
2021-12-05 08:59:54 -0400

    fix: remove doc for removed funciotn

--- lib/tesla_coil.ex
+++ lib/tesla_coil.ex
@@ -3,15 +3,6 @@ defmodule TeslaCoil do
   Documentation for `TeslaCoil`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TeslaCoil.hello()
-      :world
-
-  """
   defmacro __using__(_) do
     quote(do: use TeslaCoil.Router)
   end
