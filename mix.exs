8ad8fd8c96a6658229674a47980f79a7e9d4795e
andremaker <andr.crono@hotmail.com>
2021-12-13 02:24:17 -0400

    chore: change version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TeslaCoil.MixProject do
   def project do
     [
       app: :tesla_coil,
-      version: "0.1.0",
+      version: "0.1.2",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
